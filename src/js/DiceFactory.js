b67d49a0a25a2b3c644f7a8336a8b0a731c1330a
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-17 20:55:35 +0100

    GREEN: Add implementation to throw exception on unknown die type.

--- src/js/DiceFactory.js
+++ src/js/DiceFactory.js
@@ -7,6 +7,8 @@ export class DiceFactory {
                 return new Die(6)
             case 'd12':
                 return new Die(12)
+            default:
+                throw new Error('Unknown die type')
         }
     }
 }
