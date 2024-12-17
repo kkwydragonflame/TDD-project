8cb5e6e17246e1fb5a70c7c1d4c148b66e50d286
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-17 20:27:40 +0100

    RED: Add failing test for printing rules to console.

--- test/consoleUI.test.js
+++ test/consoleUI.test.js
@@ -14,3 +14,17 @@ test('should print welcome message to console', () => {
 
     expect(spy).toHaveBeenCalledWith('Welcome to Dicey!')
 })
+
+test('should print rules to console', () => {
+    const spy = jest.spyOn(console, 'log')
+
+    sut.rules()
+
+    expect(spy).toHaveBeenCalledWith('Rules: You\'ll start with 2 dice. You can roll additional dice, and their results will be added to your total. The goal is to get as close to, but not over, a set Max Score, using as few dice as possible.'
+)
+})
+
+// test for printing status
+// test for printing score
+// test for printing menu
+// test for printing game options
