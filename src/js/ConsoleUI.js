8723298b446bc058be0f0ceaa6ad3920751aedbe
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-17 20:28:36 +0100

    GREEN: Add method to print game rules to console.

--- src/js/ConsoleUI.js
+++ src/js/ConsoleUI.js
@@ -2,4 +2,8 @@ export class ConsoleUI {
     welcome() {
         console.log('Welcome to Dicey!')
     }
+
+    rules() {
+        console.log('Rules: You\'ll start with 2 dice. You can roll additional dice, and their results will be added to your total. The goal is to get as close to, but not over, a set Max Score, using as few dice as possible.')
+    }
 }
