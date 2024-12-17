87a47ad4e2efabd9d7db36444232a3f1f9df8c5d
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-17 20:56:49 +0100

    RED: Add failing test for creating a 20 sided die.

--- test/diceFactory.test.js
+++ test/diceFactory.test.js
@@ -19,6 +19,12 @@ test('should create a 12 sided die', () => {
     expect(die.sides).toBe(12)
 })
 
+test('should create a 20 sided die', () => {
+    const die = sut.createDice('d20')
+
+    expect(die.sides).toBe(20)
+})
+
 test('should throw error for unknown die type', () => {
     expect(() => {
         const die = sut.createDice('d9')
