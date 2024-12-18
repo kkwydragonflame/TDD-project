ce56263ccc188018683f7d8619fc5f93d7dbd6e0
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-18 23:33:02 +0100

    GREEN: Add code to print menu options to console.

--- src/js/ConsoleUI.js
+++ src/js/ConsoleUI.js
@@ -7,5 +7,10 @@ export class ConsoleUI {
         console.log('Rules: You\'ll start with 2 dice. You can roll additional dice, and their results will be added to your total.\n The goal is to get as close to, but not over, a set Max Score, using as few dice as possible.')
     }
 
-    menu() {}
+    menu() {
+        console.log('1. Start Game')
+        console.log('2. Read Rules')
+        console.log('3. High Scores')
+        console.log('4. Exit')
+    }
 }
