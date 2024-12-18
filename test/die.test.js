67dc4e84a228d37887ecb72c45c351f66af956d1
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-18 21:14:28 +0100

    GREEN: Add passing test for faulty implementation for a 20 sided die.

--- test/die.test.js
+++ test/die.test.js
@@ -1,14 +1,28 @@
 import { Die } from '../src/js/Die.js'
-import { jest, test, expect } from '@jest/globals'
+import { jest, test, expect, describe } from '@jest/globals'
 
 let sut;
 
-beforeEach(() => {
-    sut = new Die(12)
+describe('Testing 12 sided die', () => {
+    beforeEach(() => {
+        sut = new Die(12)
+    })
+
+    test('roll should return a number between 1 and 12', () => {
+        const result = sut.roll()
+        expect(result).toBeGreaterThanOrEqual(1)
+        expect(result).toBeLessThanOrEqual(12)
+    })
 })
 
-test('roll should return a number between 1 and 12', () => {
-    const result = sut.roll()
-    expect(result).toBeGreaterThanOrEqual(1)
-    expect(result).toBeLessThanOrEqual(12)
+describe('Testing 20 sided die', () => {
+    beforeEach(() => {
+        sut = new Die(20)
+    })
+
+    test('roll should return a number between 1 and 20', () => {
+        const result = sut.roll()
+        expect(result).toBeGreaterThanOrEqual(1)
+        expect(result).toBeLessThanOrEqual(20)
+    })
 })
