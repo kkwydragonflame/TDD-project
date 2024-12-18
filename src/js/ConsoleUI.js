93ce96ac2adb79f17f549f843bce8a1bf78784d8
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-18 23:32:04 +0100

    RED: Add failing test for printing menu.

--- src/js/ConsoleUI.js
+++ src/js/ConsoleUI.js
@@ -4,6 +4,8 @@ export class ConsoleUI {
     }
 
     rules() {
-        console.log('Rules: You\'ll start with 2 dice. You can roll additional dice, and their results will be added to your total. The goal is to get as close to, but not over, a set Max Score, using as few dice as possible.')
+        console.log('Rules: You\'ll start with 2 dice. You can roll additional dice, and their results will be added to your total.\n The goal is to get as close to, but not over, a set Max Score, using as few dice as possible.')
     }
+
+    menu() {}
 }
--- test/consoleUI.test.js
+++ test/consoleUI.test.js
@@ -20,13 +20,24 @@ test('should print rules to console', () => {
 
     sut.rules()
 
-    expect(spy).toHaveBeenCalledWith('Rules: You\'ll start with 2 dice. You can roll additional dice, and their results will be added to your total. The goal is to get as close to, but not over, a set Max Score, using as few dice as possible.'
+    expect(spy).toHaveBeenCalledWith('Rules: You\'ll start with 2 dice. You can roll additional dice, and their results will be added to your total.\n The goal is to get as close to, but not over, a set Max Score, using as few dice as possible.'
 )
 })
 
-// test for printing status
-// test for printing score
-// test for printing menu
-// test for printing game options
-// test for prompting user for input
-// test for displaying error message
+test('should print menu options to console', () => {
+    const spy = jest.spyOn(console, 'log')
+
+    sut.menu()
+
+    expect(spy).toHaveBeenCalledWith('1. Start Game')
+    expect(spy).toHaveBeenCalledWith('2. Read Rules')
+    expect(spy).toHaveBeenCalledWith('3. High Scores')
+    expect(spy).toHaveBeenCalledWith('4. Exit')
+})
+
+test.todo('should print status to console')
+test.todo('should print score to console')
+test.todo('should prompt user for input')
+test.todo('should display error message')
+test.todo('should clear console')
+test.todo('should exit program')
