56eb9fa7212a8685acb89e3ccc1d613f39ff40de
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-18 21:17:31 +0100

    RED: Add failing test for rolling a 4 sided die.

--- test/die.test.js
+++ test/die.test.js
@@ -3,6 +3,18 @@ import { jest, test, expect, describe } from '@jest/globals'
 
 let sut;
 
+describe('Testing 4 sided die', () => {
+    beforeEach(() => {
+        sut = new Die(4)
+    })
+
+    test('roll should return a number between 1 and 4', () => {
+        const result = sut.roll()
+        expect(result).toBeGreaterThanOrEqual(1)
+        expect(result).toBeLessThanOrEqual(4)
+    })
+})
+
 describe('Testing 12 sided die', () => {
     beforeEach(() => {
         sut = new Die(12)
