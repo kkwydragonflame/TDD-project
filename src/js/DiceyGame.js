2d51c37f4352834b7e92ee10fa2fd4fcc1a2b8c1
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-18 22:11:27 +0100

    GREEN: Add implementation to throw error when no UI is provided.

--- src/js/DiceyGame.js
+++ src/js/DiceyGame.js
@@ -1,3 +1,10 @@
 export class DiceyGame {
-
+    #ui
+    
+    constructor(ui) {
+        if (!ui) {
+            throw new Error('UserInterface is required')
+        }
+        this.ui = ui
+    }
 }
