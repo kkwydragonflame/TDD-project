74894b05071f7098fcee1b0f49ee2d1aaeb36906
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-18 22:40:09 +0100

    GREEN: Add code to throw exception when no Factory is provided for Game creation.

--- src/js/DiceyGame.js
+++ src/js/DiceyGame.js
@@ -1,10 +1,15 @@
 export class DiceyGame {
     #ui
-    
-    constructor(ui) {
+    #dieFactory
+
+    constructor(ui, dieFactory) {
         if (!ui) {
             throw new Error('UserInterface is required')
         }
+        if (!dieFactory) {
+            throw new Error('DieFactory is required')
+        }
         this.ui = ui
+        this.dieFactory = dieFactory
     }
 }
