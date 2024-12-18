26ab8ad66e58e8f952483d2608be90b19cbdc037
Johanna Eriksson <je224gs@student.lnu.se>
2024-12-18 21:16:50 +0100

    GREEN: Add code to create a 4 sided die.

--- src/js/DiceFactory.js
+++ src/js/DiceFactory.js
@@ -3,6 +3,8 @@ import { Die } from './Die.js'
 export class DiceFactory {
     createDice(type) {
         switch (type) {
+            case 'd4':
+                return new Die(4)
             case 'd6':
                 return new Die(6)
             case 'd12':
